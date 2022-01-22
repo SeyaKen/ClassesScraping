5a3edb0fc82c414753a570492a6ab7d427fcfdcd
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-01-22 16:13:12 +0900

    second commit

--- Scraping.py
+++ Scraping.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[22]:
+# In[93]:
 
 
 get_ipython().system('pip install selenium')
 
 
-# In[51]:
+# In[94]:
 
 
 get_ipython().system('pip install beautifulsoup4')
 from bs4 import BeautifulSoup
 
 
-# In[32]:
+# In[95]:
 
 
 from selenium.webdriver.common.by import By
 
 
-# In[24]:
+# In[96]:
 
 
 from selenium import webdriver
@@ -28,29 +28,30 @@ import time
 import pandas as pd
 
 
-# In[25]:
+# In[97]:
 
 
 USER = "20C3145009I"
 PASS = "EB9Qak6UfLNKYG2"
 
 
-# In[26]:
+# In[99]:
 
 
 browser = webdriver.Chrome()
-browser.implicitly_wait(3)
+time.sleep(3)
 
 
-# In[27]:
+# In[100]:
 
 
 url_login = 'https://room.chuo-u.ac.jp/ct/home'
 browser.get(url_login)
+time.sleep(3)
 print('ログインページにアクセスしました。')
 
 
-# In[33]:
+# In[101]:
 
 
 element = browser.find_element(By.ID, "username_input")
@@ -62,48 +63,51 @@ element.send_keys(PASS)
 print('フォームを送信')
 
 
-# In[38]:
+# In[102]:
 
 
 browser_form = browser.find_element(By.CLASS_NAME, "form-button")
+time.sleep(3)
 browser_form.click()
+time.sleep(3)
 print('情報を入力してログインボタンを押しました')
 
 
-# In[39]:
+# In[103]:
 
 
 url = 'https://room.chuo-u.ac.jp/ct/syllabus__search'
 browser.get(url)
+time.sleep(3)
 print(url,':アクセス完了')
 
 
-# In[40]:
+# In[106]:
 
 
 import urllib.request as req
 
 
-# In[41]:
+# In[107]:
 
 
 from selenium.webdriver.support.select import Select
 
 
-# In[44]:
+# In[108]:
 
 
 dropdown = browser.find_element(By.NAME, 'pagelen') # ② select要素を取得
 select = Select(dropdown)
 
 
-# In[45]:
+# In[109]:
 
 
 select.select_by_index(len(select.options)-1) 
 
 
-# In[47]:
+# In[110]:
 
 
 dropdown = browser.find_element(By.NAME, 'morg1') # ② select要素を取得
@@ -111,20 +115,32 @@ select = Select(dropdown)
 select.select_by_index(1)
 
 
-# In[59]:
+# In[161]:
 
 
 class_list = []
-for num in range(49):
-    elem = browser.find_element(By.CLASS_NAME, f'row{num % 2}')
-    name = elem.text
-    class_list.append(name)
+for a in range(46):
+    elem = browser.find_element(By.CLASS_NAME, 'stdlist')
+    name = elem.text.replace('\u3000', '')
+    name = name.replace(' ', ',')
+    name = name.replace('\n', ':')
+    name = name.replace('学部・研究科など,授業科目名,学期,曜日・時限,担当教員,配当年次,単位数:', '')
+    name = name + ':'
+    nu = 0
+    for num in range(len(name)):
+        if name[num] in ':':
+            class_list.append(name[nu:num].replace(':', ''))
+            nu = num
+    if a != 45:
+        browser_form = browser.find_element(By.ID, "AFHasNext")
+        time.sleep(0)
+        browser_form.click()
 
 
-# In[60]:
+# In[166]:
 
 
-print(class_list)
+print(class_list[2266])
 
 
 # In[ ]:

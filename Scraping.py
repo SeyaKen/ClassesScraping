ab9a3d7693794a3ba8073277b6f31d37cae0785c
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-01-23 15:18:22 +0900

    wasureteta

--- Scraping.py
+++ Scraping.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[93]:
+# In[66]:
 
 
 get_ipython().system('pip install selenium')
 
 
-# In[94]:
+# In[67]:
+
+
+from pprint import pprint
+
+
+# In[68]:
 
 
 get_ipython().system('pip install beautifulsoup4')
 from bs4 import BeautifulSoup
 
 
-# In[95]:
+# In[69]:
 
 
 from selenium.webdriver.common.by import By
 
 
-# In[96]:
+# In[70]:
 
 
 from selenium import webdriver
@@ -28,30 +34,30 @@ import time
 import pandas as pd
 
 
-# In[97]:
+# In[71]:
 
 
 USER = "20C3145009I"
 PASS = "EB9Qak6UfLNKYG2"
 
 
-# In[99]:
+# In[72]:
 
 
 browser = webdriver.Chrome()
 time.sleep(3)
 
 
-# In[100]:
+# In[73]:
 
 
-url_login = 'https://room.chuo-u.ac.jp/ct/home'
+url_login = "https://room.chuo-u.ac.jp/ct/home"
 browser.get(url_login)
 time.sleep(3)
-print('ログインページにアクセスしました。')
+print("ログインページにアクセスしました。")
 
 
-# In[101]:
+# In[74]:
 
 
 element = browser.find_element(By.ID, "username_input")
@@ -60,87 +66,121 @@ element.send_keys(USER)
 element = browser.find_element(By.ID, "password_input")
 element.clear()
 element.send_keys(PASS)
-print('フォームを送信')
+print("フォームを送信")
 
 
-# In[102]:
+# In[75]:
 
 
 browser_form = browser.find_element(By.CLASS_NAME, "form-button")
 time.sleep(3)
 browser_form.click()
 time.sleep(3)
-print('情報を入力してログインボタンを押しました')
+print("情報を入力してログインボタンを押しました")
 
 
-# In[103]:
+# In[76]:
 
 
-url = 'https://room.chuo-u.ac.jp/ct/syllabus__search'
+url = "https://room.chuo-u.ac.jp/ct/syllabus__search"
 browser.get(url)
 time.sleep(3)
-print(url,':アクセス完了')
+print(url,":アクセス完了")
 
 
-# In[106]:
+# In[77]:
 
 
 import urllib.request as req
 
 
-# In[107]:
+# In[78]:
 
 
 from selenium.webdriver.support.select import Select
 
 
-# In[108]:
+# In[79]:
 
 
-dropdown = browser.find_element(By.NAME, 'pagelen') # ② select要素を取得
+dropdown = browser.find_element(By.NAME, "pagelen") # ② select要素を取得
 select = Select(dropdown)
 
 
-# In[109]:
+# In[80]:
 
 
 select.select_by_index(len(select.options)-1) 
 
 
-# In[110]:
+# In[81]:
 
 
-dropdown = browser.find_element(By.NAME, 'morg1') # ② select要素を取得
+dropdown = browser.find_element(By.NAME, "morg1") # ② select要素を取得
 select = Select(dropdown)
-select.select_by_index(1)
+select.select_by_index(3)
 
 
-# In[161]:
+# In[82]:
 
 
-class_list = []
-for a in range(46):
-    elem = browser.find_element(By.CLASS_NAME, 'stdlist')
-    name = elem.text.replace('\u3000', '')
-    name = name.replace(' ', ',')
-    name = name.replace('\n', ':')
-    name = name.replace('学部・研究科など,授業科目名,学期,曜日・時限,担当教員,配当年次,単位数:', '')
-    name = name + ':'
+class_list = {}
+kai = 0
+for a in range(38):
+    elem = browser.find_element(By.CLASS_NAME, "stdlist")
+    name = elem.text.replace("\u3000", "")
+    name = name.replace(" ", "+")
+    name = name.replace("\n", ":")
+    name = name.replace("学部・研究科など+授業科目名+学期+曜日・時限+担当教員+配当年次+単位数:", "")
+    name = name + ":"
     nu = 0
     for num in range(len(name)):
-        if name[num] in ':':
-            class_list.append(name[nu:num].replace(':', ''))
+        kaiten = 0
+        if name[num] == ":":
+            name2 = name[nu:num].replace(":", "")
             nu = num
-    if a != 45:
+            kijun = 0
+            kai += 1
+            mydict = {"major": "a", "classname": "a","when": "a", "days": "a", "teacher": "a", "haitou": "a","credits": "a", "__collections__": {}}
+            for ah in range(len(name2)):
+                if name2[ah] == "+" and kaiten == 0:
+                    mydict["major"] = name2[kijun:ah].replace("+", "")
+                    kijun = ah
+                    kaiten = kaiten + 1
+                elif name2[ah] == "+" and kaiten == 1:
+                    mydict["classname"] = name2[kijun:ah].replace("+", "")
+                    kijun = ah
+                    kaiten = kaiten + 1
+                elif name2[ah] == "+" and kaiten == 2:
+                    mydict["when"] = name2[kijun:ah].replace("+", "")
+                    kijun = ah
+                    kaiten = kaiten + 1
+                elif name2[ah] == "+" and kaiten == 3:
+                    mydict["days"] = name2[kijun:ah].replace("+", "")
+                    kijun = ah
+                    kaiten = kaiten + 1
+                elif name2[ah] == "+" and kaiten == 4:
+                    mydict["teacher"] = name2[kijun:ah].replace("+", "")
+                    kijun = ah
+                    kaiten = kaiten + 1
+                elif name2[ah] == "+" and kaiten == 5:
+                    mydict["haitou"] = name2[kijun:ah].replace("+", "")
+                    kijun = ah
+                    kaiten = kaiten + 1
+                elif kaiten == 6:
+                    mydict["credits"] = name2[len(name2)-1:len(name2)]
+                    kijun = ah
+            
+            class_list[f'{kai-1}'] = mydict        
+    if a != 37:
         browser_form = browser.find_element(By.ID, "AFHasNext")
-        time.sleep(0)
         browser_form.click()
 
 
-# In[166]:
+# In[86]:
 
 
-print(class_list[2266])
+pprint(class_list)
 
 
 # In[ ]:
